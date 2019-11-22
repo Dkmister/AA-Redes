7d7f9e089e4cc30e60689fbd0164bc7f6408e868
Dkmister <vilmar.neto@hotmail.com>
2019-11-22 14:22:21 -0300

    Teste TCP funcionando :D

--- clientTCP.py
+++ clientTCP.py
@@ -4,11 +4,11 @@ import socket
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = "To each according to his abilities, to each according to his needs."
- 
+MESSAGE = "Gugu"
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
+s.send(MESSAGE.encode('utf-8'))
 data = s.recv(BUFFER_SIZE)
 s.close() 
-print "received data:", data
+print ("received data:", data)
