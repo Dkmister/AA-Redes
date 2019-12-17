31f9dc5c9dc40dfc5438a30065f54ad94f6c8690
dkmisterv <vilmar.neto@gmail.com>
2019-12-17 17:16:20 -0200

    Agora coloca o gráfico transpostos dos 3 clientes

--- log_extract.py
+++ log_extract.py
@@ -1,18 +1,46 @@
 import sys
 import matplotlib.pyplot as plt
 
-if len(sys.argv)!=2:
-	print('Usage: python3 log_extract.py filename')
+if len(sys.argv)!=4:
+	print('Usage: python3 log_extract.py filename filename filename')
 	sys.exit(1)
 	
-filename = sys.argv[1]
+filename1 = sys.argv[1]
+filename2 = sys.argv[2]
+filename3 = sys.argv[3]
 
-lst_band = []
+lst_band1 = []
+lst_band2 = []
+lst_band3 = []
 
-with open(filename,'r') as data:
+with open(filename1,'r') as data:
 	for line in data.readlines():
-		lst_band.append(float(line[10::].rstrip()))
+		lst_band1.append(float(line[10::].rstrip()))
 
-plt.plot(lst_band)
-plt.ylabel('Mbps')
+with open(filename2,'r') as data:
+	for line in data.readlines():
+		lst_band2.append(float(line[10::].rstrip()))
+
+with open(filename3,'r') as data:
+	for line in data.readlines():
+		lst_band3.append(float(line[10::].rstrip()))
+
+
+
+
+while(len(lst_band1) != len(lst_band2)):
+	lst_band2.insert(0,0.0)
+
+while(len(lst_band1) != len(lst_band3)):
+	lst_band3.insert(0,0.0)
+
+
+
+plt.plot(lst_band1)
+plt.plot(lst_band2)
+plt.plot(lst_band3)
+plt.ylabel('bps')
+plt.xlabel('tempo')
+plt.title('Banda ao longo do tempo')
 plt.show()
+
